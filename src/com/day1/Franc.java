c942c390d8d9e704087187e7805de6e43d7f53af
anniemon <seojung19@gmail.com>
2022-07-18 21:56:37 +0900

    refactor: move field and method in class Franc to Money

--- src/com/day1/Franc.java
+++ src/com/day1/Franc.java
@@ -1,17 +1,10 @@
 package com.day1;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money{
     Franc times(int multiplier){
         return new Franc(amount * multiplier);
     }
     Franc(int amount) {
         this.amount = amount;
     }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
-    }
-
 }
--- test/com/day1/FrancTest.java
+++ test/com/day1/FrancTest.java
@@ -12,4 +12,11 @@ class FrancTest {
         assertEquals(new Franc(15), five.times(3));
     }
 
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
 }
